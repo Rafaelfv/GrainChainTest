601b10b33d69822591f697018946a96e53919e78
Rafael Flores <jibaro.desarrollo@gmail.com>
2021-06-01 21:09:43 -0500

    fisrt commit; project created

---
+++ app/src/main/java/com/rafaelfv/grainchaintest/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.rafaelfv.grainchaintest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+    }
+}
